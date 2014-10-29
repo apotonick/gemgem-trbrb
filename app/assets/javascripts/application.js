31b8809af0c35ce817e2b7e0546468d747322258
Nick Sutterer <apotonick@gmail.com>
2014-10-29 20:54:11 +1100

    rails_apps_composer: front-end framework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require foundation
 //= require_tree .
+$(function() {
+  $(document).foundation();
+});
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module RailsFoundation
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # For Foundation 5
+    config.assets.precompile += %w( vendor/modernizr )
+
   end
 end
