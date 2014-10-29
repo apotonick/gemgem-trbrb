13503d9abe8a62c88d1e0c308e3674bafe1e8980
Nick Sutterer <apotonick@gmail.com>
2014-10-29 21:27:19 +1100

    bare bones rails! :)

--- Gemfile
+++ Gemfile
@@ -5,9 +5,6 @@ gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
-gem 'turbolinks'
-gem 'jbuilder', '~> 2.0'
-gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'foundation-rails'
 gem 'haml-rails'
---
+++ db/migrate/20141029101024_initial_database.rb
@@ -0,0 +1,32 @@
+class InitialDatabase < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text    :body
+      t.column  :weight, 'integer unsigned'
+
+      t.integer :deleted
+
+      t.integer :thing_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+
+    create_table :things do |t|
+      t.text :name
+      t.text :image_meta_data
+    end
+
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :confirmation_token
+      t.text   :image_meta_data
+    end
+
+    create_join_table :things, :users do |t|
+      # t.index [:thing_id, :user_id]
+      # t.index [:user_id, :thing_id]
+    end
+  end
+end
