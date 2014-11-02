1d2961c98fa7490c3c05182ef7751ec87fba825b
Nick Sutterer <apotonick@gmail.com>
2014-11-02 18:40:37 +1100

    test front page.

---
+++ test/controllers/visitors_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class IntegrationTest < ActionDispatch::IntegrationTest
+  it do
+    Thing::Create[thing: {name: "Trailblazer"}]
+    Thing::Create[thing: {name: "Descendents"}]
+
+    get "/"
+
+    # puts response.body
+    assert_select ".columns .header a", "Descendents"
+    assert_select ".columns.end .header a", "Trailblazer"
+  end
+end
