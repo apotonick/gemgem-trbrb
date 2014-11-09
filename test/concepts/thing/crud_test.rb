1f2a6a7efd3a3a6ef95f4bb194c188aa4b928c70
Nick Sutterer <apotonick@gmail.com>
2014-11-09 18:30:48 +1100

    adding Thing::Crud tests.

---
+++ test/concepts/thing/crud_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class ThingCrudTest < MiniTest::Spec
+  describe "Create" do
+    it "persists valid" do
+      thing = Thing::Create[thing: {name: "Rails", description: "Kickass web dev"}].model
+      thing.persisted?.must_equal true
+      thing.name.must_equal "Rails"
+      thing.description.must_equal "Kickass web dev"
+    end
+  end
+end
