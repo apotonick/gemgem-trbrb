748828cf530ba324a45d67ca55a98e6069cc4960
Nick Sutterer <apotonick@gmail.com>
2015-01-14 11:49:39 +1100

    use trailblazer's new crud autoloading in a railtie.

--- config/initializers/trailblazer.rb
+++ config/initializers/trailblazer.rb
@@ -1,14 +1,9 @@
-require 'trailblazer/operation'
-require 'trailblazer/operation/responder'
-require 'trailblazer/operation/representer'
-require 'trailblazer/operation/controller'
 require 'reform/form/json'
 
 require "roar/json"
 require 'roar/json/hal'
 
 require 'trailblazer/autoloading'
-require 'trailblazer/crud_autoloading'
 
 # TODO: this was handled in roar-rails. we don't need roar-rails in Trailblazer (yay!), so provide this via Trb.
 # initializer "roar.set_configs" do |app|
@@ -21,3 +16,6 @@ require 'trailblazer/crud_autoloading'
     end
   end
 # end
+
+# this is too late, apparently. the railtie is not considered anymore.
+# require 'trailblazer/rails/railtie'
