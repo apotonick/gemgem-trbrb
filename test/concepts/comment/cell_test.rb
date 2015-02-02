a8517792ac8d491db9014a487e997437505a5940
Nick Sutterer <apotonick@gmail.com>
2015-02-02 16:19:52 +1100

    add a in-depth cells test for Comment:::Grid.

--- test/concepts/comment/cell_test.rb
+++ test/concepts/comment/cell_test.rb
@@ -21,21 +21,22 @@ class CommentCellTest < Cell::TestCase
   let (:thing) do
     thing = Thing::Create[thing: {name: "Rails"}].model
 
-    Comment::Create[comment: {body: "Cool stuff!", weight: "0", user: {email: "chris@trb.org"}}, id: thing.id]
-    Comment::Create[comment: {body: "Improving.", weight: "1", user: {email: "hilz@trb.org"}}, id: thing.id]
     Comment::Create[comment: {body: "Excellent", weight: "0", user: {email: "zavan@trb.org"}}, id: thing.id]
     Comment::Create[comment: {body: "!Well.", weight: "1", user: {email: "jonny@trb.org"}}, id: thing.id]
+    Comment::Create[comment: {body: "Cool stuff!", weight: "0", user: {email: "chris@trb.org"}}, id: thing.id]
+    Comment::Create[comment: {body: "Improving.", weight: "1", user: {email: "hilz@trb.org"}}, id: thing.id]
+
     thing
   end
 
   # the comment grid.
   it do
     html = concept("comment/cell/grid", thing).(:show)
-    puts html
+    # puts html
     html = Capybara.string(html)
 
     comments = html.all(:css, ".comment")
-    comments.size.must_equal 2
+    comments.size.must_equal 3
 
     first = comments[0]
     first.find(".header").must_have_content "hilz@trb.org"
@@ -44,14 +45,21 @@ class CommentCellTest < Cell::TestCase
     first.wont_have_selector(".fi-heart")
     first[:class].wont_match /\send/
 
-    last = comments[1]
-    last.find(".header").must_have_content "chris@trb.org"
-    last.find(".header time")["datetime"].must_match /\d\d-/
-    last.must_have_content "Cool stuff!"
-    last.must_have_selector(".fi-heart")
-    last[:class].must_match /\send/ # last grid item.
+    second = comments[1]
+    second.find(".header").must_have_content "chris@trb.org"
+    second.find(".header time")["datetime"].must_match /\d\d-/
+    second.must_have_content "Cool stuff!"
+    second.must_have_selector(".fi-heart")
+    second[:class].wont_match /\send/
 
-    puts html.to_s.inspect
-    html.find("#next")["value"].must_equal "More!"
+    third = comments[2]
+    third.find(".header").must_have_content "jonny@trb.org"
+    third.find(".header time")["datetime"].must_match /\d\d-/
+    third.must_have_content "!Well."
+    third.wont_have_selector(".fi-heart")
+    third[:class].must_match /\send/ # last grid item.
+
+    # "More!"
+    html.must_have_selector("#next")
   end
 end
