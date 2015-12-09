f9e18bff43c8484dac130f4189b6d47e8ce615dd
Nick Sutterer <apotonick@gmail.com>
2015-12-10 10:19:03 +1100

    fix initial database.

--- db/migrate/20141029101024_initial_database.rb
+++ db/migrate/20141029101024_initial_database.rb
@@ -29,6 +29,14 @@ class InitialDatabase < ActiveRecord::Migration
       t.timestamps
     end
 
+    create_table :authorships do |t|
+      t.integer :user_id
+      t.integer :thing_id
+
+      t.timestamps
+    end
+
+
     create_join_table :things, :users do |t|
       # t.index [:thing_id, :user_id]
       # t.index [:user_id, :thing_id]
